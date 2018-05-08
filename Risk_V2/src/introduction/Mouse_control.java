49f60654ab8273957b071a056756cb5994444533
vanke <vankevinsuy@gmail.com>
2018-05-08 18:56:20 +0200

    e

--- Risk_V2/src/introduction/Mouse_control.java
+++ Risk_V2/src/introduction/Mouse_control.java
@@ -4,10 +4,9 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Mouse_control extends MouseAdapter{
-
+	
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		super.mouseClicked(e);
-		System.out.println(e.getButton());
 	}
 }
